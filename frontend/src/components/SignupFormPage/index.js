7505bc4385d5d7a15cf7e5cb60643469e43ba227
Andrew <andrewcohen14@gmail.com>
2021-01-27 12:41:06 -0500

    BASIC STYLING: Signup Form

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -28,48 +28,61 @@ const SignupFormPage = () => {
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <label>
-                Email
+        <div className="container flex justify-center items-center h-screen mx-auto">
+            <form onSubmit={onSubmit} className="w-1/5 p-4 bg-gray-700 text-base rounded-lg">
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <div className="p-3">
+                    <label>
+                        Email
         <input
-                    type="text"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Username
+                            type="text"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                            className="outline-none py-2 pr-4 block w-full"
+                            required
+                        />
+                    </label>
+                </div>
+                <label>
+                    Username
         <input
-                    type="text"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Password
+                        type="text"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        className="outline-none py-2 pr-4 block w-full"
+                        required
+                    />
+                </label>
+                <div className="p-3">
+                    <label>
+                        Password
         <input
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Confirm Password
+                            type="password"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            className="outline-none py-2 pr-4 block w-full"
+                            required
+                        />
+                    </label>
+                </div>
+                <div className="p-3">
+                    <label>
+                        Confirm Password
         <input
-                    type="password"
-                    value={confirmPassword}
-                    onChange={(event) => setConfirmPassword(event.target.value)}
-                    required
-                />
-            </label>
-            <button type="submit">Sign Up</button>
-        </form>
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(event) => setConfirmPassword(event.target.value)}
+                            className="outline-none py-2 pr-4 block w-full"
+                            required
+                        />
+                    </label>
+                </div>
+                <div className="p-3 pt-4 flex justify-center items-center" ></div>
+                <button type="submit" className="w-full bg-gray-300 hover:bg-pink-700 text-black font-bold py-2 px-4 mb-6 rounded">Sign Up</button>
+            </form>
+        </div>
     );
 }
 
