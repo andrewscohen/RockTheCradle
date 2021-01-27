86b5f492fead21b4ac0be9003962422da4fe01ac
Andrew <andrewcohen14@gmail.com>
2021-01-27 12:33:49 -0500

    BASIC STYLING: Login Form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import './LoginForm.css';
@@ -25,30 +25,43 @@ function LoginFormPage() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <label>
-                Username or Email
+        <div className="container flex justify-center items-center h-screen mx-auto">
+            <form onSubmit={handleSubmit} className="w-1/5 p-4 bg-gray-700 text-base rounded-lg">
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <div className="p-3">
+                    <label className="text-lg">
+                        Username or Email
         <input
-                    type="text"
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Password
+                            type="text"
+                            value={credential}
+                            onChange={(e) => setCredential(e.target.value)}
+                            className="outline-none py-2 pr-4 block w-full"
+                            required
+                        />
+                    </label>
+                </div>
+                <div className="p-3">
+                    <label>
+                        Password
         <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
-            <button type="submit">Log In</button>
-        </form>
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="outline-none py-2 pr-4 block w-full"
+                            required
+                        />
+                    </label>
+                </div>
+                <div className="p-3 pt-4 flex justify-center items-center">
+                    <button type="submit" className="w-full bg-gray-300 hover:bg-pink-700 text-black font-bold py-2 px-4 mb-6 rounded">Sign In</button>
+                </div>
+                <div className="text-xs">
+                    <Link to="/signup">DON'T HAVE AN ACCOUNT? JOIN NOW</Link>
+                </div>
+            </form>
+        </div>
     );
 }
 
