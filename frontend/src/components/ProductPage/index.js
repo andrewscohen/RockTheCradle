57567be641f1b475cf0dbe14b3da71f3fe1c3500
Andrew <andrewcohen14@gmail.com>
2021-01-29 12:20:05 -0500

    STYLING: began products page

--- frontend/src/components/ProductPage/index.js
+++ frontend/src/components/ProductPage/index.js
@@ -18,6 +18,8 @@ const ProductPage = () => {
     const [rating, setRating] = useState(0);
     const [size, setSize] = useState('');
     const [tag, setTag] = useState('');
+    const [postalCode, setPostalCode] = useState('');
+    const [inventoryNum, setInventoryNum] = useState('');
 
     useEffect(() => {
         dispatch(productActions.getOneProduct(id))
@@ -33,28 +35,80 @@ const ProductPage = () => {
             setContent(product.content)
             setRating(product.rating)
             setSize(product.Sizes)
+            setPostalCode('')
+            setInventoryNum(product.inventoryNum)
             // setTag(product.name)
         }
     }, [product])
 
-
     return (
         // Container
-        <div>
+        <div className='flex justify-evenly pt-10'>
             {product && (
                 <>
                     {/* Left Side */}
                     <div>
-                        <img src={product.productImg} alt={product.name} />
+                        <img src={product.productImg} alt={product.name} className="h-80 w-80 border-r-2 border-gray-300" />
                     </div>
-                    <div>
+                    {/* Right Side */}
+                    <div className="bg-white border-t-2 border-b-2 border-gray-300">
+                        <div className="border-b-2 border-gray-300 p-2">
+                            <h2 className="font-bold font-serif text-2xl">{product.brand}</h2>
+                            <h3 className="text-sm">{product.name}</h3>
+                            <p className="text-xs text-gray-600 pt-1">${product.retailPrice} original retail</p>
+                        </div>
+                        <div>
+                            <form>
+                                <div className="border-b-2 border-gray-300 p-2">
+                                    <label>ZIP CODE</label>
+                                    <input type='text' name='postalCode' className="border-gray-300 pb-2 border-opacity-100" />
 
+                                    <select>
+                                        <option>Select Size</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                                <p>DELIVERY + RETURN DATES</p>
+                                <div className="mt-1">
+                                    <div className="flex">
+                                        <div>
+                                            <input
+                                                id='fourDay'
+                                                type='radio'
+                                                value='fourDay'
+                                            />
+                                        </div>
+                                        <div className="pl-2 text-xs">
+                                            <label>4 Day Rental</label>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="flex">
+                                            <div>
+                                                <input
+                                                    id='fiveDay'
+                                                    type='radio'
+                                                    value='fiveDay'
+                                                />
+                                                <label className="pl-2 text-xs">5 Day Rental</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div>
+                            <button className="bg-black text-white hover:bg-opacity-75 w-full mt-2 rounded-sm text-xs p-2">Add to bag</button>
+                        </div>
                     </div>
-
-
                 </>
-            )}
-        </div>
+            )
+            }
+        </div >
     )
 }
 
