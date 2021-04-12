f05c3bd6c4cfd8d464a2e08aa1e28c9843c7b5ef
Andrew <andrewcohen14@gmail.com>
2021-04-12 16:52:42 -0400

    reverted styles

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -29,29 +29,28 @@ function Navigation({ isLoaded }) {
     } else {
         sessionLinks = (
             <>
-                <div className="flex justify-around w-full mt-7 pr-20 bg-primary">
-                    <NavLink to="/login" className="font-body text-l transition duration-100 hover:underline hover:text-gray-500 ease">
+                <div className="flex justify-around w-full pr-20 mt-7">
+                    <NavLink to="/login" className="transition duration-100 font-body text-l hover:underline hover:text-gray-500 ease">
                         Log in
                     </NavLink>
                     <p>I</p>
-                    <NavLink to="/signup" className="font-body text-l transition duration-100 hover:underline hover:text-gray-500 ease">Sign up</NavLink>
+                    <NavLink to="/signup" className="transition duration-100 font-body text-l hover:underline hover:text-gray-500 ease">Sign up</NavLink>
                 </div>
             </>
         );
     }
     return (
-        <nav className="fixed opacity-100 h-20 z-99 bg-primary grid grid-cols-10 w-screen">
-            <div className="fixed opacity-100 h-20 z-99 grid grid-cols-10 w-screen">
+        <nav className="fixed grid w-screen h-20 grid-cols-10 bg-white shadow-md opacity-100 z-99">
+            <div className="fixed grid w-screen h-20 grid-cols-10 shadow-md opacity-100 z-99">
                 <div className="col-start-1 col-end-2">
-                    <h1 className='logo'>Rock the Cradle</h1>
-                    {/* <NavLink exact to="/"><img src="https://rtcmediabucket.s3.amazonaws.com/assets/rtc-transparent.png" alt="logo" className="h-20 ml-20 transition duration-100 hover:shadow ease" /></NavLink> */}
+                    <NavLink exact to="/"><img src="https://rtcmediabucket.s3.amazonaws.com/assets/rtc-transparent.png" alt="logo" className="h-20 ml-20 transition duration-100 hover:shadow ease" /></NavLink>
                 </div>
 
-                <div className="col-start-2 col-end-4">
+                <div className="flex justify-around ml-2 mt-7 ">
                     <div className="flex justify-around mt-7">
-                        <NavLink to='/orders/1' className="font-body text-ltransition duration-100 hover:underline hover:text-gray-500 ease">My Cart</NavLink>
-                        <NavLink to='#' className="font-body text-ltransition duration-100 hover:underline hover:text-gray-500 ease">Accessories</NavLink>
-                        <NavLink to='#' className="font-body text-l transition duration-100 hover:underline hover:text-gray-500 ease">Gear</NavLink>
+                        <NavLink to='/orders/1' className="duration-100 font-body text-ltransition hover:underline hover:text-gray-500 ease">My Cart</NavLink>
+                        <NavLink to='#' className="duration-100 font-body text-ltransition hover:underline hover:text-gray-500 ease">Accessories</NavLink>
+                        <NavLink to='#' className="transition duration-100 font-body text-l hover:underline hover:text-gray-500 ease">Gear</NavLink>
                     </div>
                 </div>
                 <div className='col-start-9 col-end-11 pl-14 '>
@@ -95,13 +94,13 @@ function Navigation({ isLoaded }) {
     // );
 
     //     <div>
-    // <nav className="sticky bg-grey-600 shadow-md h-20 z-50 w-full px-5 py-2 flex justify-between items-center cursor-pointer">
+    // <nav className="sticky z-50 flex items-center justify-between w-full h-20 px-5 py-2 shadow-md cursor-pointer bg-grey-600">
     //             <div className="flex">
 
-    //                 <NavLink exact to="/" className="h-20 w-20 self-center items-center"><img src="https://rtcmediabucket.s3.amazonaws.com/assets/rtc-transparent.png" alt="logo" /></NavLink>
+    //                 <NavLink exact to="/" className="items-center self-center w-20 h-20"><img src="https://rtcmediabucket.s3.amazonaws.com/assets/rtc-transparent.png" alt="logo" /></NavLink>
     //             </div>
 
-    //             <div className="flex w-full m-auto px-20 items-center content-center">
+    //             <div className="flex items-center content-center w-full px-20 m-auto">
     //                 <div className="flex justify-between w-full pl-6">
     //                     <NavLink to='#' className="hover:underline">Clothing</NavLink>
     //                     <NavLink to='#' className="hover:underline">Accessories</NavLink>
@@ -109,7 +108,7 @@ function Navigation({ isLoaded }) {
     //                 </div>
 
     //             </div>
-    //             <div className="flex justify-end items-end space-x-3 p-1">
+    //             <div className="flex items-end justify-end p-1 space-x-3">
     //                 {isLoaded && sessionLinks}
     //             </div>
     //         </nav>
